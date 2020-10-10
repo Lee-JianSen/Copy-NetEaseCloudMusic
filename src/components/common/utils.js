b311eb22339009fba02ec29c66429f6d0dfa3ef4
L <952603659@qq.com>
2020-10-10 14:30:45 +0800

    修改防抖函数

--- src/components/common/utils.js
+++ src/components/common/utils.js
@@ -1,11 +1,12 @@
 // 防抖
 export function debounce(func, delay) {
     let timer = null;
+    let time = delay || 1000;
     return function (...args) {
         if (timer) clearTimeout(timer);
         timer = setTimeout(() => {
             func.apply(this, args)
-        }, delay)
+        }, time)
     }
 }
 
